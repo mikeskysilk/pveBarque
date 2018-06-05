9f2e1d2f08f5399f19e1c47dd9e2a39ae03a3efd
mikeskysilk <36085617+mikeskysilk@users.noreply.github.com>
2018-06-05 11:27:07 -0700

    modified AV status response, fixed snapshot deep scrubbing

--- pveBarque.py
+++ pveBarque.py
@@ -26,7 +26,7 @@ r_pw = config['redis']['password']				#Redis server password
 locations = {}									#backup storage destinations
 for option in config.options('destinations'):
 	locations[option] = config.get('destinations', option)
-version = '0.79c'
+version = '0.79d'
 starttime = None
 
 #global vars
@@ -457,9 +457,9 @@ class Worker(multiprocessing.Process):
 		# 	r.hset(vmid, 'state', 'enqueued')
 		return
 	def scrubDeep(self):
-		out = subprocess.check_output('rbd ls', shell=True)
+		out = subprocess.check_output('rbd ls {}'.format(pool.strip('/')), shell=True)
 		for disk in out.split():
-			cmd = subprocess.check_output('rbd snap ls {}'.format(disk), shell=True)
+			cmd = subprocess.check_output('rbd snap ls {}{}'.format(pool,disk), shell=True)
 			if "barque" in cmd.split():
 				vmid = disk.split('-')[1]
 				if not r.hget(vmid, 'state') == 'active':
@@ -477,6 +477,10 @@ class Worker(multiprocessing.Process):
 		return
 	def migrate(self, vmid):
 		pass
+
+####				    ####
+## pveBarque API Classes ##
+####			 	    ####
 
 class Backup(Resource):
 	@auth.login_required
@@ -786,7 +790,7 @@ class AVtoggle(Resource):
 					print("disabling antivirus on node: {}, output: {}".format(node, cmd))
 				except:
 					return{'state':'error'},200
-				return {'state':'disabled'},200
+				return {'state':'disabling'},200
 			if request.args['switch'] == 'on':
 				node = request.args['node']
 				try:
@@ -802,16 +806,23 @@ class AVtoggle(Resource):
 		if 'node' not in request.args:
 			return {'error': 'Node argument required beacuse... it needs a node'}, 400
 		node = request.args['node']
+		active = None
 		try:
 			print("node: {}".format(node))
 			cmd= subprocess.check_output("ssh -t root@{} '/opt/sophos-av/bin/savdstatus'".format(node), shell=True)
-			return{'status':cmd.strip()},200
+			active = cmd.strip()
 		except subprocess.CalledProcessError as e:
-			return {'status':e.output.strip()},200
+			active = e.output.strip()
 		except:
 			print("error getting status")
-			return{'error':'problem getting status'},200
-			
+			return{'error':'problem getting status'},500
+		if 'scanning is running' in active:
+			return{'active':True, 'status':active}, 200
+		elif 'scanning is not running' in active:
+			return{'active':False, 'status':active}, 200
+		else:
+			return{'error':"Problem determining AV status"}, 500
+
 api.add_resource(ListAllBackups, '/barque/')
 api.add_resource(ListBackups, '/barque/<int:vmid>')
 api.add_resource(Backup, '/barque/<int:vmid>/backup')
